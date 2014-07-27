106ba904cc85373b93b439c747bac67af6bc5c04
Hiroki Ishiura <ishiura@ja2.so-net.ne.jp>
2014-07-27 15:31:46 +0900

    Added a progress handler for uploading.

--- URLConnectionSample/ViewController.m
+++ URLConnectionSample/ViewController.m
@@ -13,7 +13,7 @@
 @interface URLConnection : NSObject
 
 - (NSData *)sendSynchronousRequest:(NSURLRequest *)request returningResponse:(NSURLResponse *__autoreleasing *)response error:(NSError *__autoreleasing *)error;
-- (NSData *)sendSynchronousRequestWithProgressReport:(NSURLRequest *)request returningResponse:(NSURLResponse *__autoreleasing *)response error:(NSError *__autoreleasing *)error progressHandler:(BOOL (^)(float progress))progressHandler;
+- (NSData *)sendSynchronousRequestWithProgressReport:(NSURLRequest *)request returningResponse:(NSURLResponse *__autoreleasing *)response error:(NSError *__autoreleasing *)error uploadProgressHandler:(BOOL (^)(float progress))uploadProgressHandler downloadProgressHandler:(BOOL (^)(float progress))downloadProgressHandler;
 
 @end
 
@@ -25,7 +25,8 @@
 @property (strong, nonatomic) NSURLResponse *response;
 @property (strong, nonatomic) NSMutableData *result;
 @property (strong, nonatomic) NSError *error;
-@property (copy, nonatomic) BOOL (^progressHandler)(float progress);
+@property (copy, nonatomic) BOOL (^uploadProgressHandler)(float progress);
+@property (copy, nonatomic) BOOL (^downloadProgressHandler)(float progress);
 
 @property (strong, nonatomic) NSURLConnection *connection;
 @property (assign, nonatomic) BOOL done;
@@ -42,7 +43,8 @@
 		_response = nil;
 		_result = nil;
 		_error = nil;
-		_progressHandler = nil;
+		_uploadProgressHandler = nil;
+		_downloadProgressHandler = nil;
 		_connection = nil;
     }
     return self;
@@ -54,7 +56,8 @@
 	_response = nil;
 	_result = nil;
 	_error = nil;
-	_progressHandler = nil;
+	_uploadProgressHandler = nil;
+	_downloadProgressHandler = nil;
 	_connection = nil;
 }
 
@@ -63,44 +66,54 @@
 	return [NSURLConnection sendSynchronousRequest:request returningResponse:response error:error];
 }
 
-- (NSData *)sendSynchronousRequestWithProgressReport:(NSURLRequest *)request returningResponse:(NSURLResponse *__autoreleasing *)response error:(NSError *__autoreleasing *)error progressHandler:(BOOL (^)(float progress))progressHandler
+- (NSData *)sendSynchronousRequestWithProgressReport:(NSURLRequest *)request returningResponse:(NSURLResponse *__autoreleasing *)response error:(NSError *__autoreleasing *)error uploadProgressHandler:(BOOL (^)(float progress))uploadProgressHandler downloadProgressHandler:(BOOL (^)(float progress))downloadProgressHandler
 {
 	_request = request;
 	_response = nil;
 	_result = [NSMutableData data];
 	_error = nil;
-	_progressHandler = progressHandler;
+	_uploadProgressHandler = uploadProgressHandler;
+	_downloadProgressHandler = downloadProgressHandler;
 	_done = NO;
 	
-	[NSThread detachNewThreadSelector:@selector(startDownload) toTarget:self withObject:nil];
+	[NSThread detachNewThreadSelector:@selector(startTask) toTarget:self withObject:nil];
 	while (!_done) {
 		[NSThread sleepForTimeInterval:0.05];
 	}
 	
 	_connection = nil;
 	_request = nil;
-	_progressHandler = nil;
+	_uploadProgressHandler = nil;
+	_downloadProgressHandler = nil;
 	
 	*response = _response;
 	*error = _error;
 	return _result;
 }
 
-- (void)startDownload
+- (void)startTask
 {
 	@autoreleasepool {
 		_connection = [[NSURLConnection alloc] initWithRequest:_request delegate:self startImmediately:NO];
 		[_connection start];
+		
+		if (_request.HTTPBody && _request.HTTPBody.length > 0) {
+			if (_uploadProgressHandler && !_uploadProgressHandler(0.0)) {
+				[self cancelTask];
+				return;
+			}
+		}
+		
 		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate distantFuture]];
 	}
 }
 
-- (void)cancelDownload
+- (void)cancelTask
 {
 	[_connection cancel];
 	
 	NSDictionary *info = @{
-		NSLocalizedDescriptionKey: @"Downloading is canceled by user action.",
+		NSLocalizedDescriptionKey: @"Request is canceled by user action.",
 		NSURLErrorFailingURLErrorKey: _request.URL,
 		NSURLErrorFailingURLStringErrorKey: _request.URL.absoluteString,
 	};
@@ -109,18 +122,36 @@
 	[NSThread exit];
 }
 
-- (void)endDownload:(NSError *)error
+- (void)endTask:(NSError *)error
 {
 	_error = error;
 	_done = YES;
 	[NSThread exit];
 }
 
+- (void)connection:(NSURLConnection *)connection didSendBodyData:(NSInteger)bytesWritten totalBytesWritten:(NSInteger)totalBytesWritten totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite
+{
+	float progress;
+	if (totalBytesExpectedToWrite > 0) {
+		progress = (double)totalBytesWritten / (double)totalBytesExpectedToWrite;
+		if (progress < 0.0 || progress > 1.0) {
+			// 0.0 -> on startTask
+			return;
+		}
+	} else {
+		progress = NAN;
+	}
+	
+	if (_uploadProgressHandler && !_uploadProgressHandler(progress)) {
+		[self cancelTask];
+	}
+}
+
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
 	_response = response;
-	if (!_progressHandler(0.0)) {
-		[self cancelDownload];
+	if (_downloadProgressHandler && !_downloadProgressHandler(0.0)) {
+		[self cancelTask];
 	}
 }
 
@@ -140,23 +171,27 @@
 		}
 	}
 	
-	if (!_progressHandler(progress)) {
-		[self cancelDownload];
+	if (_downloadProgressHandler && !_downloadProgressHandler(progress)) {
+		[self cancelTask];
 	}
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-	if (_progressHandler(1.0)) {
-		[self endDownload:nil];
+	if (_downloadProgressHandler) {
+		if (_downloadProgressHandler(1.0)) {
+			[self endTask:nil];
+		} else {
+			[self cancelTask];
+		}
 	} else {
-		[self cancelDownload];
+		[self endTask:nil];
 	}
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
-	[self endDownload:error];
+	[self endTask:error];
 }
 
 @end
@@ -181,7 +216,7 @@
 {
     [super viewDidLoad];
 	
-	_urlTextField.text = @"http://";
+	_urlTextField.text = @"http://localhost/";
 	_resultLabel.text = @"Ready.";
 	_progressView.progress = 0.0;
 	
@@ -266,8 +301,8 @@
 		URLConnection *connection = [[URLConnection alloc] init];
 		NSHTTPURLResponse *response = nil;
 		NSError *error = nil;
-		NSData *result = [connection sendSynchronousRequestWithProgressReport:request returningResponse:&response error:&error progressHandler:^BOOL(float progress) {
-			NSLog(@"progress=%f", progress);
+		NSData *result = [connection sendSynchronousRequestWithProgressReport:request returningResponse:&response error:&error uploadProgressHandler:nil downloadProgressHandler:^BOOL(float progress) {
+			NSLog(@"download progress=%f", progress);
 			if (!isnan(progress)) {
 				dispatch_async(self.mainQueue, ^{
 					_progressView.progress = progress;
@@ -294,7 +329,66 @@
 	});
 }
 
-- (IBAction)cancelDownloadAsync:(id)sender
+- (IBAction)uploadAsync:(id)sender
+{
+	_progressView.progress = 0.0;
+	_resultLabel.text = @"Started.";
+	
+	_cancel = NO;
+	NSLog(@"enqueued a task.");
+	dispatch_async(self.commandQueue, ^{
+		NSLog(@"started a task.");
+		
+		NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:_urlTextField.text]];
+		request.cachePolicy = NSURLRequestReloadIgnoringCacheData;
+		request.timeoutInterval = 10.0;
+		request.HTTPMethod = @"POST";
+		NSMutableData *body = [NSMutableData data];
+		{
+			NSString *boundary = @"743bbc65af0e5364cc6e0fc50787794c";
+			NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+			[request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+			[body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+			[body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"data\"; filename=\"upload.dat\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+			[body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+			[body appendData:(NSData *)[[NSMutableData alloc] initWithLength:1024*1024*10]];
+			[body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+		}
+		request.HTTPBody = (NSData *)body;
+		
+		URLConnection *connection = [[URLConnection alloc] init];
+		NSHTTPURLResponse *response = nil;
+		NSError *error = nil;
+		NSData *result = [connection sendSynchronousRequestWithProgressReport:request returningResponse:&response error:&error uploadProgressHandler:^BOOL(float progress) {
+			NSLog(@"upload progress=%f", progress);
+			if (!isnan(progress)) {
+				dispatch_async(self.mainQueue, ^{
+					_progressView.progress = progress;
+				});
+			}
+			return !_cancel;
+		} downloadProgressHandler:nil];
+	
+		NSLog(@"response = %@", response.description);
+		NSLog(@"result.length = %ld", (long)(result ? result.length : 0));
+		NSLog(@"error = %@", error.description);
+		
+		NSString *message;
+		if (response.statusCode == 200 && !error && result.length > 0) {
+			message = @"Finished.";
+		} else {
+			message = @"Aborted.";
+		}
+		
+		connection = nil;
+		dispatch_async(self.mainQueue, ^{
+			_resultLabel.text = message;
+		});
+		NSLog(@"finished the task.");
+	});
+}
+
+- (IBAction)cancel:(id)sender
 {
 	_cancel = YES;
 }
